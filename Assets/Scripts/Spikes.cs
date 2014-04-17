2cf97d8eb5724798b673dec750ca9864ebc8ab06
fdhesse <ococo@hotmail.com>
2014-04-17 23:52:20 +0800

    Florian changes on 17 April
    created the spikes
    removed some old levels unnecessary

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Monobehaviour for the game object inside which the game should take place, can be a cube or anything else.
+/// The Pawn is considered out of bounds when it moves out of the GameSpace object.
+/// </summary>
+public class Spikes : MonoBehaviour {
+
+    void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.tag == "Player" )
+	        c.gameObject.GetComponent<Pawn>().outOfBounds();
+		else
+			Debug.Log( "Cube reached DeadZone" );
+    }
+}
