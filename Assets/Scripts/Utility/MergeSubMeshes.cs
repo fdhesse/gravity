48389a740831ea1d9dcdb5ac7f70e6544e01b779
Alban Nanty <banban@banban.org>
2015-11-23 22:28:47 +0800

    + add a script to merge submeshes (mainly for the stair model) - remove the anim import and avatar creation from most of the imported assets - remove the anim component from most of the prefab of the deco

---
+++ Assets/Scripts/Utility/MergeSubMeshes.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MergeSubMeshes : MonoBehaviour
+{
+	// Use this for initialization
+	void Awake()
+	{
+		MeshFilter meshFilter = transform.GetComponent<MeshFilter>();
+		int subMeshCount = meshFilter.sharedMesh.subMeshCount;
+		CombineInstance[] combine = new CombineInstance[subMeshCount];
+		for (int i = 0 ; i < subMeshCount; ++i)
+		{
+			combine[i].mesh = meshFilter.sharedMesh;
+			combine[i].subMeshIndex = i;
+		}
+
+		// get the current active state of the object and desactive it
+		bool activeSelfState = meshFilter.gameObject.activeSelf;
+		meshFilter.gameObject.SetActive(false);
+		// combine the sub meshes
+		meshFilter.mesh.CombineMeshes(combine, true, false);
+		// reset the active state
+		transform.gameObject.SetActive(activeSelfState);
+	}
+}
