cf61ceeceaf88d48582d23c7d01a8ff82280c2e1
Alban Nanty <banban@banban.org>
2016-02-05 16:06:33 +0800

    | change the rendering of the cylinder gizmo to be able to transform into a sphere

--- Assets/Editor/CameraTargetEditor.cs
+++ Assets/Editor/CameraTargetEditor.cs
@@ -43,7 +43,6 @@ public class CameraTargetEditor : Editor
 	{
 		const int SECTION_COUNT = 64;
 		const int CONE_EDGE_COUNT = 4;
-		const int CYLINDER_EDGE_COUNT = 8;
 		Vector3 position = src.transform.position;
 		Quaternion rotation = src.transform.rotation;
 
@@ -67,58 +66,56 @@ public class CameraTargetEditor : Editor
 			break;
 
 		case CameraTarget.AngleConstraint.CYLINDER:
-			float cylinderRadius = size;
+			float cylinderRadius = size * Mathf.Cos(src.tiltLimitAngle * Mathf.Deg2Rad);
 			float cylinderHeight = size * Mathf.Sin(src.tiltLimitAngle * Mathf.Deg2Rad);
-			float halfCylinderHeight = cylinderHeight * 0.5f;
 			// set the matrix in the center of the object
 			Gizmos.matrix = Matrix4x4.TRS(position, rotation, Vector3.one);
-			// draw the tow circles
-			drawFlatEllipse(SECTION_COUNT, 0, cylinderRadius, cylinderRadius, halfCylinderHeight);
-			Vector3[] bottomPoints = drawFlatEllipse(SECTION_COUNT, CYLINDER_EDGE_COUNT, cylinderRadius, cylinderRadius, -halfCylinderHeight);
-			// draw the side lines
-			Vector3 cylinderTranslation = new Vector3(0f, cylinderHeight, 0f);
-			for (int i = 0; i < bottomPoints.Length; ++i)
-				Gizmos.DrawLine(bottomPoints[i], bottomPoints[i] + cylinderTranslation);
+			// draw the tow cap circles
+			drawCircleArc(SECTION_COUNT, 0f, 360f, cylinderRadius, cylinderHeight, 1);
+			drawCircleArc(SECTION_COUNT, 0f, 360f, cylinderRadius, -cylinderHeight, 1);
+			// draw the side circles
+			drawCircleArc(SECTION_COUNT/2, -src.tiltLimitAngle, src.tiltLimitAngle, size, 0f, 0);
+			drawCircleArc(SECTION_COUNT/2, 180f - src.tiltLimitAngle, 180f + src.tiltLimitAngle, size, 0f, 0);
+			drawCircleArc(SECTION_COUNT/2, 90f - src.tiltLimitAngle, 90f + src.tiltLimitAngle, size, 0f, 2);
+			drawCircleArc(SECTION_COUNT/2, 270f - src.tiltLimitAngle, 270f + src.tiltLimitAngle, size, 0f, 2);
 			break;
 		}
 	}
 
-	static Vector3[] drawFlatEllipse(int segments, int returnedPointCount, float xradius, float zradius, float y)
+	static void drawCircleArc(int segments, float startAngle, float endAngle, float radius, float planeConstant, int planeIndex)
 	{
-		float x = 0f;
-		float z = 0f;
-		Vector3 previousPosition = new Vector3(0f, y, zradius);
-		float angleIncrement = (360f / segments);
-		float angle = angleIncrement;
+		// rotating angle increment
+		float angleIncrement = ((endAngle - startAngle) / segments);
+		float angle = startAngle + angleIncrement;
 
-		Vector3[] returnedPoints = null;
-		int pointGapCount = 0;
-		if (returnedPointCount > 0)
-		{
-			returnedPoints = new Vector3[returnedPointCount];
-			pointGapCount = segments / returnedPointCount;
-		}
+		// compute the first previous position
+		Vector3 previousPosition = computeArcPosition(startAngle, radius, planeConstant, planeIndex);
 
 		for (int i = 0; i < segments; i++)
 		{
-			//store the points in the return array if needed
-			if ((returnedPointCount > 0) && ((i % pointGapCount) == 0))
-				returnedPoints[i / pointGapCount] = previousPosition;
-
 			// compute the new position
-			x = Mathf.Sin (Mathf.Deg2Rad * angle) * xradius;
-			z = Mathf.Cos (Mathf.Deg2Rad * angle) * zradius;
-
-			Vector3 newPosition = new Vector3(x, y, z);
+			Vector3 newPosition = computeArcPosition(angle, radius, planeConstant, planeIndex);
 			Gizmos.DrawLine(previousPosition, newPosition);
 			previousPosition = newPosition;
 
 			angle += angleIncrement;
 		}
-
-		return returnedPoints;
 	}
-	
+
+	static private Vector3 computeArcPosition(float angle, float radius, float planeConstant, int planeIndex)
+	{
+		// compute the new position
+		float firstCoord = Mathf.Sin (Mathf.Deg2Rad * angle) * radius;
+		float secondCoord = Mathf.Cos (Mathf.Deg2Rad * angle) * radius;
+		
+		if (planeIndex == 0)
+			return new Vector3(planeConstant, firstCoord, secondCoord);
+		else if (planeIndex == 1)
+			return new Vector3(firstCoord, planeConstant, secondCoord);
+		else
+			return new Vector3(firstCoord, secondCoord, planeConstant);
+	}
+
 	static Vector3[] drawEllipse(int segments, int returnedPointCount, float xradius, float yradius, float distance)
 	{
 		float squaredDistance = distance*distance;
