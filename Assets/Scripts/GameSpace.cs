98a1d466cd6fd7ed3847d7c5c98bff94b9330a82
Alban Nanty <banban@banban.org>
2016-02-22 15:55:31 +0800

    + add some assert in the GameSpace script

--- Assets/Scripts/GameSpace.cs
+++ Assets/Scripts/GameSpace.cs
@@ -5,7 +5,30 @@ using System.Collections;
 /// Monobehaviour for the game object inside which the game should take place, can be a cube or anything else.
 /// The Pawn is considered out of bounds when it moves out of the GameSpace object.
 /// </summary>
-public class GameSpace : MonoBehaviour {
+[RequireComponent(typeof(Collider))]
+public class GameSpace : MonoBehaviour
+{
+	void Awake()
+	{
+		// get the bounds of this game space
+		Bounds bound = this.GetComponent<Collider>().bounds;
+
+		// make sure all the falling cube and the pawn is inside the game space,
+		// otherwise log a warning, because it can creates bugs later
+		GameObject pawn = GameObject.FindGameObjectWithTag("Player");
+		if (pawn != null)
+		{
+			Debug.Assert(bound.Contains(pawn.transform.position), "WARNING: The player pawn is OUTSIDE the GameSpace limit. When the player falls out of the game, it will not be detected and the player won't respawn! Please enlarge the GameSpace bounding box.");
+		}
+
+		// also check that all falling cubes are inside the game space,
+		// because pawn cannot move until all falling cube has finished to fall
+		GameObject[] fallingCubes = GameObject.FindGameObjectsWithTag("FallingCube");
+		foreach (GameObject cube in fallingCubes)
+		{
+			Debug.Assert(bound.Contains(cube.transform.position), "WARNING: A falling cube is OUTSIDE the GameSpace limit. When the cube falls out of the game, it will not be detected and the player won't be able to move anymore! Please enlarge the GameSpace bounding box.");
+		}
+	}
 
     void OnTriggerExit(Collider c)
 	{
