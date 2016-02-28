5a866da2102b087c8944e380e91edadb488d3f31
fdhesse <ococo@hotmail.com>
2016-02-28 23:43:26 +0800

    polish on several stuff

---
+++ Assets/Scripts/UI/LevelSelection.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelection : MonoBehaviour 
+{
+	public void ChangeScene(string sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+}
