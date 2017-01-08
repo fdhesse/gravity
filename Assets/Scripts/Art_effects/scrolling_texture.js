c20d0e3b5d26e767d298f0d5ed76891275c72cf1
Florian Dhesse <fdhesse@gmail.com>
2017-01-09 01:22:27 +0800

    lava fall

--- Assets/Scripts/Art_effects/scrolling_texture.js
+++ Assets/Scripts/Art_effects/scrolling_texture.js
@@ -6,7 +6,7 @@ var scrollSpeed2 = 2.00;
 
 function FixedUpdate() {
 
-	var offset = Time.time * scrollSpeed;
-	var offset2 = Time.time * scrollSpeed2;
+    var offset = Time.time * scrollSpeed * Random.Range(1, 2);
+    var offset2 = Time.time * scrollSpeed2 * Random.Range(1, 2);
 	GetComponent.<Renderer>().material.mainTextureOffset = Vector3 (0,offset2,0);
 }
