312858961d350cf1da71092f939d9ea93a577e3d
Abel Dantas <dantas.abel@gmail.com>
2017-03-22 16:20:51 +0000

    Add VFX for lightning bolt
    Only gets triggered when player changes gravity,
    doesn't play on up tiles

--- Assets/Unity Essentials Procedural Examples/Sources/LightningBolt.cs
+++ Assets/Unity Essentials Procedural Examples/Sources/LightningBolt.cs
@@ -1,66 +1,93 @@
-/*
-	This script is placed in public domain. The author takes no responsibility for any possible harm.
-	Contributed by Jonathan Czeck
-*/
+using System;
 using UnityEngine;
-using System.Collections;
 
 public class LightningBolt : MonoBehaviour
 {
-	public Transform target;
-	public int zigs = 100;
-	public float speed = 1f;
-	public float scale = 1f;
-	public Light startLight;
-	public Light endLight;
-	
-	Perlin noise;
-	float oneOverZigs;
-	
-	private Particle[] particles;
+    public Transform target;
+    public int zigs = 100;
+    public float speed = 1f;
+    public float scale = 1f;
+
+    Perlin noise;
+    float oneOverZigs;
+
+    private Particle[] particles;
 
     ParticleEmitter particleEmitter;
+    ParticleRenderer particleRenderer;
 
-    void Start()
-	{
-		oneOverZigs = 1f / (float)zigs;
+    bool playing;
+
+    public void Start()
+    {
+        oneOverZigs = 1f / zigs;
         particleEmitter = GetComponent<ParticleEmitter>();
+        particleRenderer = GetComponent<ParticleRenderer>();
         particleEmitter.emit = false;
 
-		particleEmitter.Emit(zigs);
-		particles = particleEmitter.particles;
-	}
-	
-	void Update ()
-	{
-		if (noise == null)
-			noise = new Perlin();
-			
-		float timex = Time.time * speed * 0.1365143f;
-		float timey = Time.time * speed * 1.21688f;
-		float timez = Time.time * speed * 2.5564f;
-		
-		for (int i=0; i < particles.Length; i++)
-		{
-			Vector3 position = Vector3.Lerp(transform.position, target.position, oneOverZigs * (float)i);
-			Vector3 offset = new Vector3(noise.Noise(timex + position.x, timex + position.y, timex + position.z),
-										noise.Noise(timey + position.x, timey + position.y, timey + position.z),
-										noise.Noise(timez + position.x, timez + position.y, timez + position.z));
-			position += (offset * scale * ((float)i * oneOverZigs));
-			
-			particles[i].position = position;
-			particles[i].color = Color.white;
-			particles[i].energy = 1f;
-		}
-		
-		particleEmitter.particles = particles;
-		
-		if (particleEmitter.particleCount >= 2)
-		{
-			if (startLight)
-				startLight.transform.position = particles[0].position;
-			if (endLight)
-				endLight.transform.position = particles[particles.Length - 1].position;
-		}
-	}	
+        particleEmitter.Emit( zigs );
+        particles = particleEmitter.particles;
+        particleRenderer.enabled = false;
+    }
+
+    public void Update()
+    {
+        if ( !playing )
+        {
+            return;
+        }
+
+        if ( noise == null )
+            noise = new Perlin();
+
+        var timex = Time.time * speed * 0.1365143f;
+        var timey = Time.time * speed * 1.21688f;
+        var timez = Time.time * speed * 2.5564f;
+
+        for ( int i = 0; i < particles.Length; i++ )
+        {
+            var position = Vector3.Lerp( transform.position, target.position, oneOverZigs * i );
+            var offset = new Vector3( noise.Noise( timex + position.x, timex + position.y, timex + position.z ),
+                noise.Noise( timey + position.x, timey + position.y, timey + position.z ),
+                noise.Noise( timez + position.x, timez + position.y, timez + position.z ) );
+            position += offset * scale * ( i * oneOverZigs );
+
+            particles[i].position = position;
+            particles[i].color = Color.white;
+            particles[i].energy = 1f;
+        }
+
+        particleEmitter.particles = particles;
+    }
+
+    public void Stop()
+    {
+        if ( particleEmitter != null )
+        {
+            particleEmitter.enabled = false;
+            particleEmitter.emit = false;
+        }
+        if ( particleRenderer != null )
+        {
+            particleRenderer.enabled = false;
+        }
+        playing = false;
+    }
+
+    public void Play( Tile focusedTile )
+    {
+        if ( focusedTile != null )
+        {
+            if ( focusedTile.orientation != TileOrientation.Up )
+            {
+                target = focusedTile.gameObject.transform;
+                playing = true;
+                particleRenderer.enabled = true;
+            }
+        }
+        else
+        {
+            Debug.LogError( "Playing SFX on unkown tile" );
+        }
+    }
 }
