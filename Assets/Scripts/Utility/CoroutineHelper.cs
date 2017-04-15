dca03704406da4a14d05379f45b068a5655511c8
Abel Dantas <dantas.abel@gmail.com>
2017-04-15 18:12:44 +0100

    Add coroutine utility

---
+++ Assets/Scripts/Utility/CoroutineHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class CoroutineHelper : MonoBehaviour {
+    public static IEnumerator ContinuouslyDo( Action action )
+    {
+        while ( true )
+        {
+            action();
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    public static IEnumerator DoEvery( float step, Action action )
+    {
+        while ( true )
+        {
+            action();
+            yield return new WaitForSeconds(step);
+        }
+    }
+
+    public static IEnumerator WaitAndDo( float delay, Action action )
+    {
+        yield return new WaitForSeconds( delay );
+        action();
+    }
+
+}
