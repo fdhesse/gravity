3bd82f0f9f29428098b5eeb73c5c257da0e11418
Abel Dantas <dantas.abel@gmail.com>
2017-04-15 18:18:25 +0100

    Add gravity reactive waterfall
     * Editor script to edit Waterfall Monobehaviours and test this
     * All the needed animations and animator assigned to the waterfall
     * Example on Temple_Entrance_Tool
     * Editor utility Animanipulator to rotate and reverse animations
     * Prefab for waterfalls at PREFABS/Waterfall/

---
+++ Assets/Scripts/Utility/Editor/ClipInfo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+// Editor window for listing all object reference curves in an animation clip
+public class ClipInfo : EditorWindow
+{
+    private AnimationClip clip;
+
+    [MenuItem( "Mu/Clip Info" )]
+    static void Init()
+    {
+        GetWindow( typeof( ClipInfo ) );
+    }
+
+    public void OnGUI()
+    {
+        clip = EditorGUILayout.ObjectField( "Clip", clip, typeof( AnimationClip ), false ) as AnimationClip;
+        
+        EditorGUILayout.LabelField( "Curves:" );
+        if ( clip != null )
+        {
+            foreach ( var binding in AnimationUtility.GetCurveBindings( clip ) )
+            {
+                AnimationCurve curve = AnimationUtility.GetEditorCurve( clip, binding );
+                EditorGUILayout.LabelField( binding.path + "/" + binding.propertyName + ", Keys: " + curve.keys.Length );
+            }
+        }
+
+        EditorGUILayout.LabelField( "Object reference curves:" );
+        if ( clip != null )
+        {
+            foreach ( var binding in AnimationUtility.GetObjectReferenceCurveBindings( clip ) )
+            {
+                ObjectReferenceKeyframe[] keyframes = AnimationUtility.GetObjectReferenceCurve( clip, binding );
+                EditorGUILayout.LabelField( binding.path + "/" + binding.propertyName + ", Keys: " + keyframes.Length );
+            }
+        }
+    }
+}
