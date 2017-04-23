6774525a534605eecce9a8a8b6c1ab72261c0ded
Abel Dantas <dantas.abel@gmail.com>
2017-04-23 08:01:34 +0100

    Add Bezier helper method

---
+++ Assets/Scripts/Utility/MathUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class MathUtils {
+    public static Vector3 CubicBezierCurve( Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t )
+    {
+        var r = 1f - t;
+        var f0 = r * r * r;
+        var f1 = r * r * t * 3;
+        var f2 = r * t * t * 3;
+        var f3 = t * t * t;
+        return f0 * p0 + f1 * p1 + f2 * p2 + f3 * p3;
+    }
+}
