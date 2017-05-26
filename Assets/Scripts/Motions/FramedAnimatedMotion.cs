33c7599431f9934c24ae71ebee36ff0a68659abc
Abel Dantas <dantas.abel@gmail.com>
2017-05-26 21:44:03 +0000

    Remove motion editor scripts

---
+++ Assets/Scripts/Motions/FramedAnimatedMotion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedMotionFrame
+{
+    public int Frame;
+    public Vector3 Translation;
+    public Vector3 Rotation;
+}
+
+
+public class FramedAnimatedMotion : AnimatedMotion
+{
+    public List<AnimatedMotionFrame> AnimatedMotionFrames;
+}
