c108380ec652ae2f05f8ab10926321b70c0752ab
Alban Nanty <banban@banban.org>
2017-09-29 21:59:50 +0800

    | use 2 emitters (clone the existing one) and switch between the two when the gravity change.

--- Assets/Scripts/Actor/Waterfall.cs
+++ Assets/Scripts/Actor/Waterfall.cs
@@ -6,6 +6,11 @@ public class Waterfall : MonoBehaviour
 	[Tooltip("The particle system that generate the water particles")]
 	private ParticleSystem m_WaterEmitter = null;
 
+	// we need a copy of the Water emiter to make a smooth transition when we change gravity
+	private ParticleSystem m_DuplicatedWaterEmitter = null;
+
+	private ParticleSystem m_CurrentEmitterPlaying = null;
+
 	// save the 3 size over time curve, that we intervert during gravity change
 	private ParticleSystem.MinMaxCurve m_SizeCurveForWidth;
 	private ParticleSystem.MinMaxCurve m_SizeCurveAlongGravity;
@@ -18,17 +23,26 @@ public class Waterfall : MonoBehaviour
 		m_SizeCurveForWidth = m_WaterEmitter.sizeOverLifetime.x;
 		m_SizeCurveAlongGravity = m_WaterEmitter.sizeOverLifetime.y;
 		m_SizeCurveAlongEmissionDirection = m_WaterEmitter.sizeOverLifetime.z;
+
+		// duplicate the emitter
+		m_DuplicatedWaterEmitter = Instantiate(m_WaterEmitter, m_WaterEmitter.transform.parent);
+
+		// and take a reference on the first emitter as the current one
+		m_CurrentEmitterPlaying = m_WaterEmitter;		
 	}
 
 	public void Reset(TileOrientation startingOrientation)
 	{
 		// for now simply change the orientation
 		ChangeGravity(startingOrientation);
+		StartCurrentEmitter(true);
 	}
 
 	public void ChangeGravity(TileOrientation orientation)
 	{
-		var sizeOverLifetime = m_WaterEmitter.sizeOverLifetime;
+		SwitchEmitter();
+
+		var sizeOverLifetime = m_CurrentEmitterPlaying.sizeOverLifetime;
 
 		// intervert the size over life time curves depending on the orientation of the gravity
 		switch (orientation)
@@ -54,5 +68,22 @@ public class Waterfall : MonoBehaviour
 				sizeOverLifetime.z = m_SizeCurveAlongGravity;
 				break;
 		}
+	}
+
+	private void SwitchEmitter()
+	{
+		// stop the current emitter and let the particle die
+		m_CurrentEmitterPlaying.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+		// switch
+		m_CurrentEmitterPlaying = (m_CurrentEmitterPlaying == m_WaterEmitter) ? m_DuplicatedWaterEmitter : m_WaterEmitter;
+		// start the new emitter
+		StartCurrentEmitter(false);
+	}
+
+	private void StartCurrentEmitter(bool usePrewarm)
+	{
+		var mainModule = m_CurrentEmitterPlaying.main;
+		mainModule.prewarm = usePrewarm;
+		m_CurrentEmitterPlaying.Play();
 	}
 }
